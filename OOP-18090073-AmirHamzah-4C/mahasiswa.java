d3ee4f519895d26dbd6f603f96e7cb276224710c
hamzahart <48015310+hamzahart@users.noreply.github.com>
2020-03-22 10:44:32 +0700

    Add files via upload

---
+++ OOP-18090073-AmirHamzah-4C/mahasiswa.java
@@ -0,0 +1,11 @@
+public class mahasiswa{
+    public static void main(String[] args) {
+        String nama = "Amir Hamzah"
+        String kelas = "4C"
+        String nim = "18090073"
+
+        System.out.println("nama"+nama);
+        System.out.println("kelas"+kelas);
+        System.out.println("nim"+nim);
+    }
+}
